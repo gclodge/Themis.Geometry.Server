5de26f7c9bf56d5830412ea60900da3457eb25b5
gclodge <graham.clodge@gmail.com>
2022-03-19 14:38:12 -0400

    Added MediatR

--- src/Themis.Geometry.Server/Program.cs
+++ src/Themis.Geometry.Server/Program.cs
@@ -1,9 +1,12 @@
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMediatR(System.Reflection.Assembly.GetExecutingAssembly());
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
